0feb39d50bd35c3f1216166a4ea2fef7a57a768c
Utakato <copyresearch98@gmail.com>
2022-01-22 16:13:29 +0200

    Add prints to see where it crashes

--- main.py
+++ main.py
@@ -9,15 +9,26 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_NAME = os.environ.get("DB_NAME")
 
-client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.flhu6.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
-
-db = client["crypto"]
-raw_data_db = db["raw_data"]
-
 
 if __name__ == '__main__':
-    db_entires = helper.get_all_db_entries(raw_data_db)
+    print('Program started!')
+    client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.flhu6.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+    db = client["crypto"]
+    raw_data_db = db["raw_data"]
+    print("Connected to DB!")
 
+    print("Start fetching db entries!")
+    db_entires = helper.get_all_db_entries(raw_data_db)
+    print("Fetched all entries.")
+    
+    print("Proceeding to get coins")
     scrapper.update_DB(scrapper.get_coins(0,9), raw_data_db)
+    print("Got all coins!")
+
+    print("Preparing for watchlists")
     watchlists.update_all_coins_watchlist(db_entires, raw_data_db)
+    print("Got all watchlists!")
+
+    print("Moving on to twitter")
     twitter.update_followers(db_entires, raw_data_db)
+    print("All done!")
